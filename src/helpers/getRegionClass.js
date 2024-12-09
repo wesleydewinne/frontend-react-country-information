c2c0d6abe8c7064d3e127d3e2547bb464ce0da19
wesleydewinne <wesleydewinne@gmail.com>
2024-12-09 21:38:57 +0100

    Opdrachten afgerond, helaas heb ik ok bij de uitwerkingen gekeken omdat ik afgelopen week niet de tijd heb kunnen nemen om het huiswerk af te kunnen ronden.

---
+++ src/helpers/getRegionClass.js
@@ -0,0 +1,18 @@
+function getRegionClass(currentRegion) {
+    switch (currentRegion) {
+        case 'Africa':
+            return 'blue';
+        case 'Americas':
+            return 'green';
+        case 'Asia':
+            return 'red';
+        case 'Europe':
+            return 'yellow';
+        case 'Oceania':
+            return 'purple';
+        default:
+            return 'default';
+    }
+}
+
+export default getRegionClass;
